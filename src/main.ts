dd5c1ce3d94893a9496c9f626b505690026dfb23
darius molotokas <dariusm.it12@gmail.com>
2024-09-12 12:57:05 +0300

    working on local cache

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ interface weatherType {
 }
 
 let weatherList: weatherType[] = [];
+let savedWeatherList: weatherType[] = [];
 let weatherId: number = 0;
 let currentPageGlobal: number = 1;
 let itemsPerPageGlobal: number = 3;
@@ -26,48 +27,53 @@ const testArrayButton: HTMLElement = document.getElementById("test-array")!;
 const testPagination: HTMLElement = document.getElementById("test-pages")!;
 const prevPageButton: HTMLElement = document.getElementById("prev-page")!;
 const nextPageButton: HTMLElement = document.getElementById("next-page")!;
-const searchInput: HTMLElement = <HTMLInputElement>(
-  document.getElementById("search-input")
-);
+const searchInput = <HTMLInputElement>document.getElementById("search-input");
 let searchInputValue: string = "";
 
 async function getWeatherData(): Promise<void> {
-  const api_key = import.meta.env.VITE_API_KEY;
-  const api_url =
-    "https://api.openweathermap.org/data/2.5/weather?units=metric";
-  let city = searchInputValue;
-  try {
-    const { data } = await axios.get(
-      api_url + `&q=${city}` + `&appid=${api_key}`
-    );
+  if (searchInputValue === "") {
+    alert("Please enter city name first");
+  } else {
+    const api_key = import.meta.env.VITE_API_KEY;
+    const api_url =
+      "https://api.openweathermap.org/data/2.5/weather?units=metric";
+    let city = searchInputValue;
+    try {
+      const { data } = await axios.get(
+        api_url + `&q=${city}` + `&appid=${api_key}`
+      );
 
-    const sunriseUnix: number = data.sys.sunrise;
-    const sunsetUnix: number = data.sys.sunset;
-    const sunrise: Object = new Date(sunriseUnix * 1000);
-    const sunset: Object = new Date(sunsetUnix * 1000);
+      const sunriseUnix: number = data.sys.sunrise;
+      const sunsetUnix: number = data.sys.sunset;
+      const sunrise: Object = new Date(sunriseUnix * 1000);
+      const sunset: Object = new Date(sunsetUnix * 1000);
 
-    //Add data from API to local array
-    weatherList.push({
-      id: (weatherId = weatherId + 1),
-      city: data.name,
-      country: data.sys.country,
-      temp: data.main.temp,
-      humidity: data.main.humidity,
-      windspeed: data.wind.speed,
-      pressure: data.main.pressure,
-      sunrise: sunrise.toString(),
-      sunset: sunset.toString(),
-      conditions: data.weather[0].icon,
-    });
-    constructList();
-  } catch (error: any) {
-    if (error.response) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log("Error", error.message);
+      savedWeatherList =
+        savedWeatherList.length > 0 ? savedWeatherList : weatherList;
+
+      //Add data from API to local array
+      savedWeatherList.push({
+        id: (weatherId = weatherId + 1),
+        city: data.name,
+        country: data.sys.country,
+        temp: data.main.temp,
+        humidity: data.main.humidity,
+        windspeed: data.wind.speed,
+        pressure: data.main.pressure,
+        sunrise: sunrise.toString(),
+        sunset: sunset.toString(),
+        conditions: data.weather[0].icon,
+      });
+      constructList();
+    } catch (error: any) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
     }
   }
 }
@@ -75,9 +81,11 @@ async function getWeatherData(): Promise<void> {
 //Save to local storage
 function saveToStorage() {
   localStorage.setItem("weather data", weatherListDiv.innerHTML);
+  localStorage.setItem("weather array", JSON.stringify(weatherList));
 }
 
 function renderList() {
+  savedWeatherList = JSON.parse(localStorage.getItem("weather array") || "[]");
   weatherListDiv.innerHTML = localStorage.getItem("weather data") || "";
 }
 
@@ -113,6 +121,11 @@ function constructList() {
       " Sunset: " +
       item.sunset;
     deleteButton.innerHTML = "Delete";
+    deleteButton.classList.add("delete-item");
+    deleteButton.setAttribute("id", String(item.id));
+    deleteButton.onclick = function () {
+      deleteItem(item.id);
+    };
     weatherItemDiv.appendChild(img);
     weatherItemDiv.appendChild(p);
     weatherItemDiv.appendChild(deleteButton);
@@ -146,6 +159,14 @@ function nextPage() {
   console.log("next page button clicked");
 }
 
+//Remove item from the array, but not from view
+function deleteItem(clickedID: number): void {
+  console.log("Item with the id of: " + clickedID + " was clicked!");
+  weatherList = weatherList.filter((item) => {
+    return item.id !== clickedID;
+  });
+}
+
 //Event listeners
 searchInput.addEventListener("change", updateValue);
 searchButton.addEventListener("click", getWeatherData);
@@ -157,18 +178,14 @@ nextPageButton.addEventListener("click", nextPage);
 testPagination.addEventListener("click", (): void => {
   paginate(currentPageGlobal, itemsPerPageGlobal);
 });
-//Remove list item Event listener
-weatherListDiv.addEventListener(
-  "click",
-  function (e) {
-    let element = e.target as HTMLElement;
-    if (element.tagName === "BUTTON") {
-      element.parentElement!.remove();
-      saveToStorage();
-    }
-  },
-  false
-);
+//Remove list item Event listener from view only
+weatherListDiv.addEventListener("click", function (e) {
+  let element = e.target as HTMLElement;
+  if (element.tagName === "BUTTON") {
+    element.parentElement!.remove();
+    saveToStorage();
+  }
+});
 
 //Call list render function
 renderList();
