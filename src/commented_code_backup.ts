d5f579da791d210787c728395ec1ca2fe802688a
darius molotokas <dariusm.it12@gmail.com>
2024-09-14 11:48:47 +0300

    saving progress

---
+++ src/commented_code_backup.ts
@@ -0,0 +1,54 @@
+//let paginatedSavedWeatherList: weatherType[] = [];
+//let currentPageGlobal: number = 1;
+//let itemsPerPageGlobal: number = 3;
+/*
+const testPaginationButton: HTMLElement =
+  document.getElementById("test-pages")!;
+const prevPageButton: HTMLElement = document.getElementById("prev-page")!;
+const nextPageButton: HTMLElement = document.getElementById("next-page")!;
+*/
+
+//const testArrayButton: HTMLElement = document.getElementById("test-array")!;
+
+/*
+function paginate(currentPage: number, itemsPerPage: number) {
+  for (let i = 0; i < savedWeatherList.length; i++) {
+    if (
+      i >= (currentPage - 1) * itemsPerPage &&
+      i < currentPage * itemsPerPage
+    ) {
+      paginatedSavedWeatherList = savedWeatherList.filter((item) => {
+        return item.id === savedWeatherList[i].id;
+      });
+    }
+  }}
+
+function prevPage() {
+  currentPageGlobal = currentPageGlobal - 1;
+  console.log("prev page button clicked");
+  console.log("CurrentPage: " + currentPageGlobal);
+  paginate(currentPageGlobal, itemsPerPageGlobal);
+}
+
+function nextPage() {
+  currentPageGlobal = currentPageGlobal + 1;
+  console.log("next page button clicked");
+  console.log("CurrentPage: " + currentPageGlobal);
+  paginate(currentPageGlobal, itemsPerPageGlobal);
+}
+*/
+
+/*
+prevPageButton.addEventListener("click", prevPage);
+nextPageButton.addEventListener("click", nextPage);
+testPaginationButton.addEventListener("click", () => {
+  //paginate(currentPageGlobal, itemsPerPageGlobal);
+  console.log(JSON.stringify(paginatedSavedWeatherList, undefined, 2));
+});
+*/
+
+/*
+testArrayButton.addEventListener("click", (): void => {
+  console.log(JSON.stringify(savedWeatherList, undefined, 2));
+});
+*/
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-//import "https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css";
 import axios from "axios";
 
 interface weatherType {
@@ -14,23 +13,16 @@ interface weatherType {
   conditions: string;
 }
 
-//let weatherList: weatherType[] = [];
-let savedWeatherList: weatherType[] = [];
-//let paginatedSavedWeatherList: Array<Object> = [];
-//let weatherId: number = 0;
-let savedWeatherId: number;
-let currentPageGlobal: number = 1;
-let itemsPerPageGlobal: number = 3;
-
-const testPaginationButton: HTMLElement =
-  document.getElementById("test-pages")!;
+let savedWeatherArray: weatherType[] = [];
 const searchButton: HTMLElement = document.getElementById("search-button")!;
 let weatherListDiv: HTMLElement = document.getElementById("weather-list")!;
-const testArrayButton: HTMLElement = document.getElementById("test-array")!;
-const prevPageButton: HTMLElement = document.getElementById("prev-page")!;
-const nextPageButton: HTMLElement = document.getElementById("next-page")!;
 const searchInput = <HTMLInputElement>document.getElementById("search-input");
 let searchInputValue: string = "";
+
+window.onload = () => {
+  renderList();
+  getArrayData();
+};
 
 async function getWeatherData(): Promise<void> {
   if (searchInputValue === "") {
@@ -44,25 +36,13 @@ async function getWeatherData(): Promise<void> {
       const { data } = await axios.get(
         api_url + `&q=${city}` + `&appid=${api_key}`
       );
-
       const sunriseUnix: number = data.sys.sunrise;
       const sunsetUnix: number = data.sys.sunset;
       const sunrise: Object = new Date(sunriseUnix * 1000);
       const sunset: Object = new Date(sunsetUnix * 1000);
-
-      savedWeatherList =
-        localStorage.getItem("weather array") === null
-          ? []
-          : JSON.parse(localStorage.getItem("weather array") || "[]");
-
-      savedWeatherId =
-        localStorage.getItem("weather id") === null
-          ? 0
-          : Number(localStorage.getItem("weather id"));
-
-      //Add data from API to local array
-      savedWeatherList.push({
-        id: (savedWeatherId = savedWeatherId + 1),
+      getArrayData();
+      savedWeatherArray.push({
+        id: savedWeatherArray.length,
         city: data.name,
         country: data.sys.country,
         temp: data.main.temp,
@@ -74,7 +54,6 @@ async function getWeatherData(): Promise<void> {
         conditions: data.weather[0].icon,
       });
       saveArrayToStorage();
-      saveWeatherId();
       constructList();
     } catch (error: any) {
       if (error.response) {
@@ -89,42 +68,21 @@ async function getWeatherData(): Promise<void> {
   }
 }
 
-function saveWeatherId() {
-  localStorage.setItem("weather id", String(savedWeatherId));
-}
-
-//Save view to local storage
-function saveViewToStorage() {
-  localStorage.setItem("weather data", weatherListDiv.innerHTML);
-}
-
-//Save array to local storage
 function saveArrayToStorage() {
-  localStorage.setItem("weather array", JSON.stringify(savedWeatherList));
+  localStorage.setItem("weather array", JSON.stringify(savedWeatherArray));
 }
 
-function getArrayData() {
-  savedWeatherList = JSON.parse(localStorage.getItem("weather array") || "[]");
-}
-
-function renderList() {
-  weatherListDiv.innerHTML = localStorage.getItem("weather data") || "";
-}
-
-//Render weather forecasts list
 function constructList() {
-  const div: HTMLElement = document.createElement("div");
-  const p: HTMLParagraphElement = document.createElement("p");
-  const deleteButton: HTMLElement = document.createElement("button");
-  const img: HTMLImageElement = document.createElement("img");
-
-  savedWeatherList.forEach((item) => {
+  getArrayData();
+  let div: HTMLElement = document.createElement("div");
+  let p: HTMLParagraphElement = document.createElement("p");
+  let deleteButton: HTMLElement = document.createElement("button");
+  let img: HTMLImageElement = document.createElement("img");
+  savedWeatherArray.forEach((item) => {
     div.setAttribute("id", `item-` + item.id);
-    div.classList.add("list-item");
     weatherListDiv.appendChild(div);
     let weatherItemDiv = document.querySelector(`#item-${item.id}`)!;
     img.src = `${item.conditions}` + `.png`;
-    img.classList.add("conditions-img");
     p.textContent =
       "City: " +
       item.city +
@@ -143,77 +101,174 @@ function constructList() {
       " Sunset: " +
       item.sunset;
     deleteButton.innerHTML = "Delete";
-    deleteButton.classList.add("delete-item");
     deleteButton.setAttribute("id", String(item.id));
-    deleteButton.onclick = function () {
-      deleteItem(item.id);
-    };
     weatherItemDiv.appendChild(img);
     weatherItemDiv.appendChild(p);
     weatherItemDiv.appendChild(deleteButton);
   });
   saveViewToStorage();
-  //paginate(currentPageGlobal, itemsPerPageGlobal);
+  renderList();
 }
 
-//What type is EVENT?
+function saveViewToStorage() {
+  localStorage.setItem("weather data", weatherListDiv.innerHTML);
+}
+
+function getArrayData() {
+  savedWeatherArray = JSON.parse(localStorage.getItem("weather array") || "[]");
+}
+
+function renderList() {
+  weatherListDiv.innerHTML = localStorage.getItem("weather data") || "";
+}
+
 function updateValue(e: any): void {
   searchInputValue = e.target.value.toLowerCase();
 }
 
-function paginate(currentPage: number, itemsPerPage: number) {
-  for (let i = 0; i < savedWeatherList.length; i++) {
-    if (
-      i >= (currentPage - 1) * itemsPerPage &&
-      i < currentPage * itemsPerPage
-    ) {
-      console.log(savedWeatherList[i]);
-    }
-  }
-}
-
-function prevPage() {
-  currentPageGlobal = currentPageGlobal - 1;
-  console.log("prev page button clicked");
-}
-
-function nextPage() {
-  currentPageGlobal = currentPageGlobal + 1;
-  console.log("next page button clicked");
-}
-
-//Remove item from the array, but not from view
-function deleteItem(clickedID: number): void {
-  console.log("Item with the id of: " + clickedID + " was clicked!");
-  savedWeatherList = savedWeatherList.filter((item) => {
-    return item.id !== clickedID;
-  });
-  saveArrayToStorage();
-}
-
-//Event listeners
 searchInput.addEventListener("change", updateValue);
 searchButton.addEventListener("click", getWeatherData);
-testArrayButton.addEventListener("click", (): void => {
-  console.log(JSON.stringify(savedWeatherList, undefined, 2));
-});
-prevPageButton.addEventListener("click", prevPage);
-nextPageButton.addEventListener("click", nextPage);
-testPaginationButton.addEventListener("click", () => {
-  paginate(currentPageGlobal, itemsPerPageGlobal);
-});
 
-//Remove list item Event listener from view only
 weatherListDiv.addEventListener("click", function (e) {
   let element = e.target as HTMLElement;
   if (element.tagName === "BUTTON") {
+    console.log("Clicked element.id:" + element.id);
     element.parentElement!.remove();
-    saveViewToStorage();
-    saveArrayToStorage();
+    deleteItem(Number(element.id));
   }
 });
 
-//Get saved weather data from local storage
-getArrayData();
-//Call list render function
-renderList();
+function deleteItem(clickID: number): void {
+  console.log("Item with the id of: " + clickID + " was deleted!");
+  savedWeatherArray = savedWeatherArray.filter((item) => {
+    return item.id !== clickID;
+  });
+  //Reindex array
+  for (let i = 0; i < savedWeatherArray.length; i++) {
+    savedWeatherArray[i].id = i;
+  }
+  weatherListDiv.innerHTML = "";
+  saveViewToStorage();
+  saveArrayToStorage();
+  constructList();
+}
+//=========================================================================================================
+//Search already added forecast functionality below
+//=========================================================================================================
+const searchAlreadyAddedButton = <HTMLButtonElement>(
+  document.getElementById("search-button-from-cache")
+);
+const searchAlreadyAddedInput = <HTMLInputElement>(
+  document.getElementById("search-input-from-cache")
+);
+let searchAlreadyAddedInputValue: string = "";
+
+function updateAlreadyAddedSearchValue(e: any): void {
+  searchAlreadyAddedInputValue = e.target.value.toLowerCase();
+}
+
+searchAlreadyAddedInput.addEventListener(
+  "change",
+  updateAlreadyAddedSearchValue
+);
+
+searchAlreadyAddedButton.addEventListener("click", buildArrayFromAlreadyAdded);
+
+let foundValuesArray: Array<{}> = [];
+let foundValueObject: Object = {};
+
+function buildArrayFromAlreadyAdded() {
+  console.log("Search input value: " + searchAlreadyAddedInputValue);
+  for (let i = 0; i < savedWeatherArray.length; i++) {
+    if (
+      savedWeatherArray[i].city.toLowerCase() ==
+      searchAlreadyAddedInputValue.toLowerCase()
+    ) {
+      if (Object.keys(foundValueObject).length !== 0) {
+        console.log(
+          "Object is not null so we need to check if it's equal to previously found value"
+        );
+        if (doesValueExist() === true) {
+          console.log("Trying to add the value that already exists");
+          break;
+        } else {
+          console.log(
+            "Values are not equal so we can add found value to the array"
+          );
+          foundValueObject = savedWeatherArray[i];
+          foundValuesArray.push(foundValueObject);
+          for (let i = 0; i < savedWeatherArray.length; i++) {
+            savedWeatherArray[i].id = i;
+          }
+          displayForecasts();
+        }
+      } else {
+        console.log("Object is null so we can just assign found value to it");
+        foundValueObject = savedWeatherArray[i];
+        foundValuesArray.push(foundValueObject);
+        for (let i = 0; i < savedWeatherArray.length; i++) {
+          savedWeatherArray[i].id = i;
+        }
+        displayForecasts();
+      }
+    }
+  }
+  console.log("Whole array:" + JSON.stringify(foundValuesArray, undefined, 2));
+}
+
+function doesValueExist(): boolean {
+  let isValueFound = false;
+  foundValuesArray.forEach((item) => {
+    //Typescript error because of incorrect type? Property does exist when code runs. Vite won't build with this error probably...
+    if (item.city.toLowerCase() == searchAlreadyAddedInputValue.toLowerCase()) {
+      console.log("doesValueExist function output: " + true);
+      isValueFound = true;
+    }
+  });
+  if (isValueFound) {
+    return true;
+  } else {
+    console.log("doesValueExist function output: " + false);
+    return false;
+  }
+}
+
+let weatherListAlreadyAddedDiv: HTMLElement = document.getElementById(
+  "weather-list-already-added"
+)!;
+
+function displayForecasts() {
+  let div: HTMLElement = document.createElement("div");
+  let p: HTMLParagraphElement = document.createElement("p");
+  let deleteButton: HTMLElement = document.createElement("button");
+  let img: HTMLImageElement = document.createElement("img");
+  foundValuesArray.forEach((item) => {
+    div.setAttribute("id", `item-cached-` + item.id);
+    weatherListAlreadyAddedDiv.appendChild(div);
+    let weatherItemDiv = document.querySelector(`#item-cached-${item.id}`)!;
+    img.src = `${item.conditions}` + `.png`;
+    p.textContent =
+      "City: " +
+      item.city +
+      " Country: " +
+      item.country +
+      " Temp: " +
+      item.temp +
+      " Humidity: " +
+      item.humidity +
+      " Wind speed: " +
+      item.windspeed +
+      " Pressure: " +
+      item.pressure +
+      " Sunrise: " +
+      item.sunrise +
+      " Sunset: " +
+      item.sunset;
+    deleteButton.innerHTML = "Delete";
+    deleteButton.setAttribute("id", String(item.id));
+    weatherItemDiv.appendChild(img);
+    weatherItemDiv.appendChild(p);
+    weatherItemDiv.appendChild(deleteButton);
+  });
+}
+//=========================================================================================================
